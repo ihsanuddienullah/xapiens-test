99b009f73d4b3c277b1c47f6a88100b0efe73f32
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-01-10 14:14:40 +0700

    fix answer 1

--- soal1.js
+++ soal1.js
@@ -1,6 +1,5 @@
 /* 
 Sebuah angka 1234567 Buatlah output :
-
 1000000
 200000
 30000
@@ -10,11 +9,12 @@ Sebuah angka 1234567 Buatlah output :
 7  
 */
 
-const angka = 1234567;
+let angka = 1234567;
 const angkaString = angka.toString();
 let zero = '0'.repeat(angkaString.length);
 
 for (let i = 0; i < angkaString.length; i++) {
     zero = zero.substring(0, zero.length - 1);
-    console.log(angkaString[i] + zero);
+    angka = parseInt(angkaString[i] + zero);
+    console.log(angka);
 }
